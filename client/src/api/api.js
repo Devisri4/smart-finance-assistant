d3bdaa7a2c4a50cee8b3038b916b5e0a93fc529f
Devi Sri <mdevisri@2004gmail.com>
2025-12-05 19:36:11 +0530

    Fix: use environment variable for API base URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,10 +1,9 @@
-// client/src/api/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-finance-api-qb1c.onrender.com",
-
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
+
 
